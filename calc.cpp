9ff4ae949836db86879e6f9b50f232b2e6a8df60
caogh1 <caogh1@lenovo.com>
2017-04-11 14:17:24 +0800

    init repo

---
+++ calc.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: calc.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年04月11日 星期二 14时14分35秒
+ ************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    cout<<"desktop calc running...."<<endl;
+
+    return 0;
+}
