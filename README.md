1f83aed658bd5c05df52b1ec653937c0dbcee5b4
caogh1 <caogh1@lenovo.com>
2017-04-11 16:45:00 +0800

    update code

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # calc_repo
 calc_repo
+
+该桌面计算器提供了四种作为浮点数的中缀运算符的标准算术运算。
+所接受的语言的语法：
+
+program:
+    END
+    expr_list END
+
+expr_list:
+	expression PRINT       //PRINT是分号
+	expression PRINT expr_list
+
+expression:
+	expression + term
+	expression - term
+	term
+	
+term:
+	term / primary
+	term * primary
+	primary
+
+primary:
+	NUMBER
+	NAME
+	NAME = expression
+	- primary
+	(expression)		
+
+　　一个程序是由分号分隔的一系列表达式。表达式的基本单元是数，名字和运算符*,/,+,-(包括一元的和二元的)和=.
+　　名字在使用前无需声明。
+　　语法分析采用通常称为递归下降的风格，这是一种很流行的直截了当的自顶向下技术。对于语法中的每个产生式存在着一个函数，它还要调用其他的函数。
+终结符(例如END,NUMBER,+和-)由词法分析程序get_token()识别，而非终结符则由语法分析函数expr(),term()和prim()识别。一旦一个(子)表达式的
+两个运算对象都已经知道了，就立即对这个表达式求值。
+	分析器利用函数get_token()取得输入，最后一次调用get_token()得到的值可以在全局变量curr_tok里找到。curr_tok的类型是枚举Token_value。
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    
--- calc.cpp
+++ calc.cpp
@@ -6,8 +6,35 @@
  ************************************************************************/
 
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
+
+//单词(token)用它们的字符所对应的整数表示.
+enum Token_value
+{
+	NAME, NUMBER, END,
+	PLUS='+', MINUS='-', MUL='*', DIV='/',
+	PRINT=';', ASSIGN='=', LP='(', RP=')'
+};
+
+Token_value curr_tok = PRINT;
+
+
+double number_value;
+string string_value;
+
+map<string, double> table;
+//每个分析函数都有一个bool参数，指明该函数是否需要调用get_token()去取得下一个单词。每个分析函数都将对它的表达式求值并返回这个值。
+
+double term(bool get);  //乘和除
+double expr(bool get);  //expr()处理加和减。它由一个查找被加减的项的循环组成.
+double prim(bool get);
+
+Token_value get_token();
+
+double error(const string& msg);
 
 
 int main(int argc, char** argv)
@@ -16,3 +43,97 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+double expr(bool get)  //加和减
+{
+	
+	double left = term(get);
+	
+	for(;;)
+	{
+		switch(curr_tok)
+		{
+		case PLUS:
+			left += term(true);
+			break;
+		case MINUS:
+			left -= term(true);
+			break;
+		default:
+			return left;
+		}
+	}
+}
+
+double term(bool get)
+{
+	double left = prim(get);
+	for(;;)
+	{
+		switch(curr_tok)
+		{
+		case MUL:
+			left *= prim(true);
+			break;
+		case DIV:
+			if(double d = prim(true))
+			{
+				left /= d;
+				break;
+			}
+			return error("divide by 0");
+		default:
+			return left;
+		}
+	}
+}
+
+double prim(bool get)  //处理初等项
+{
+	if(get) get_token();
+	switch(curr_tok)
+	{
+		case NUMBER:
+		{
+			double v = number_value;
+			get_token();
+			return v;
+		}
+		case NAME:
+		{
+			double& v = table[string_value];
+			if(get_token() == ASSIGN)
+			{
+				v = expr(true);
+			}
+			return v;
+		}
+		case MINUS:
+			return -prim(true);
+		case LP:
+		{
+			double e = expr(true);
+			if(curr_tok != RP)
+			{
+				return error(") expected");
+			}
+			get_token();   //吃掉 )
+			return e;
+		}
+		default:
+			return error("primary expected");
+	}
+}
+
+//去读一个字符，根据它决定需要去组合的是哪种单词，而后返回表示被读单词的Token_value值
+Token_value get_token()
+{
+	char ch = 0;
+	cin>>ch;
+}
+
+double error(const string& msg)
+{
+	return 0;
+}
+
